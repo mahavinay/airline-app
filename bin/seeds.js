f644f30a3cfa347094b09163eed1cc39b3a29338
abbie <abbie@summers.gg>
2020-11-16 15:43:27 +0100

    const ticket models

--- bin/seeds.js
+++ bin/seeds.js
@@ -2,13 +2,34 @@ const mongoose = require("mongoose");
 const Ticket = require("../models/ticket.model");
 
 mongoose.connect(`mongodb://localhost/${DB_NAME}`, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
 
+const tickets = [
+  {
+    date: 1 / 12 / 2020,
+    origin: "Amsterdam",
+    destination: "Kalamazoo",
+    quantity: 2,
+  },
+  {
+    date: 17 / 12 / 2020,
+    origin: "Atlanta",
+    destination: "Beijing",
+    quantity: 4,
+  },
+  {
+    date: 19 / 12 / 2020,
+    origin: "Dubai",
+    destination: "Madrid",
+    quantity: 1,
+  }
+]
+
 Ticket.create(ticket)
-.then((dronesFromDB) => {
+  .then((dronesFromDB) => {
     console.log(`Created ${dronesFromDB.length} ticket`);
     mongoose.connection.close();
   })
