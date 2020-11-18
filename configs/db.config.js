b9a0ef1c45d3c606b052de61e19bf1fe3a675809
Abbie <abbie@summers.gg>
2020-11-18 22:55:33 +0100

    revert to working db config

--- configs/db.config.js
+++ configs/db.config.js
@@ -1,15 +1,19 @@
-const mongoose = require('mongoose');
+// this line will have to be added to future seed scripts if you want to support reading the connection string from .env
+require('dotenv').config();
+const mongoose = require("mongoose");
+
 const DB_NAME = "IronAirlines"
-const MONGO_URI= `mongodb://localhost/${DB_NAME}`
-
-
-mongoose
-  .connect(process.env.MONGO_CONNECTION_STRING || MONGO_URI, {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  })
+if (process.env.MONGO_CONNECTION_STRING === undefined){
+  console.log("please set MONGO_CONNECTION_STRING environment variable.")
+  
+} else {
+mongoose.connect(`${process.env.MONGO_CONNECTION_STRING}/${DB_NAME}`, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
   .then(x =>
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   )
   .catch(err => console.error('Error connecting to mongo', err));
+}
